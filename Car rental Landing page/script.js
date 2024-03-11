e1214defa3ef394393d097f0db27080778aa281d
piyush22F <129437436+piyush22F@users.noreply.github.com>
2024-03-11 16:57:23 +0530

    Add files via upload

---
+++ Car rental Landing page/script.js
@@ -0,0 +1,14 @@
+let manu = document.querySelector(".manu-bar");
+let sideBar = document.querySelector(".nav-side-bar");
+let counter = 0;
+
+manu.addEventListener("click", function () {
+  //   console.log(manu);
+  if (counter == 0) {
+    sideBar.style.display = "flex";
+    counter++;
+  } else {
+    sideBar.style.display = "none";
+    counter--;
+  }
+});
